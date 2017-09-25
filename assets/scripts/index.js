61fcbdc2a2bd935b99ad8d8624d4c3967304e985
katwu90 <katwu90@gmail.com>
2017-09-25 09:26:41 -0400

    Add event listeners to the index.js file
    Add event listeners and change the divs and links to hide or show on
    the initial view of website.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,7 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const authEvents = require('./auth/authEvents')
 
 $(() => {
   setAPIOrigin(location, config)
@@ -12,3 +13,25 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+
+$(() => {
+  $('.restaurant-crude').hide()
+  $('.signout-link').hide()
+  $('.managepassword-link').hide()
+  $('.auth').hide()
+  $('.signup-link').on('click', () => {
+    $('.auth').hide()
+    $('.sign-up-nav').show('slow')
+  })
+  $('.signin-link').on('click', () => {
+    $('.auth').hide()
+    $('.sign-in-nav').show('slow')
+  })
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#change-password').on('submit', authEvents.onChangePassword)
+  $('.signout-link').on('click', authEvents.onSignOut)
+  $('.managepassword-link').on('click', () => {
+    $('.change-password-nav').show('slow')
+  })
+})
